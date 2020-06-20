4291e1900774720e397e907425c9026be36abde5
chiarasabaini <chiara@sabaini.com>
2020-06-20 12:24:20 +0200

    types conversion part

---
+++ 02_types/convert.go
@@ -0,0 +1,23 @@
+/*
+convert
+
+__author__ = "chiara@sabaini.com"
+__version__ = "01.01"
+__date__ = "2020-06-19"
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	j := 2.8
+
+	sum := i + int(j) // int(j) -> converts j from float to int
+	fmt.Println(sum)
+
+	k := 10
+	var l float64 = float64(k) // doesn't work without explicit conversion
+	fmt.Println("l", l)
+}
